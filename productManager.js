025d75b16f3972905d4dc6bcb2a6580d348b8b75
fmalbran93 <fmalbran93@gmail.com>
2023-07-31 21:31:47 -0300

    Agrego metodos solicitados eliminar,actualizar, traer los actualizados, a su vez funcion loadProductsFromFile, metodo SaveProductToFile, etc

--- productManager.js
+++ productManager.js
@@ -1,12 +1,13 @@
+import { promises as fs } from 'fs';
+
 class ProductManager {
   constructor(products = []) {
     this.products = products;
     this.nextId = 1;
   }
 
-  addProduct(product) {
-    const { title, description, price, thumbnail, code, stock } = product;
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
+  async addProduct(product) {
+    if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
       console.error('Todos los campos del producto son obligatorios.');
       return;
     }
@@ -19,71 +20,99 @@ class ProductManager {
     product.id = this.nextId++;
     this.products.push(product);
     console.log('Producto agregado correctamente:', product);
+    await this.saveProductsToFile();
   }
 
   getProducts() {
     return this.products;
   }
 
-  getProductById(id) {
-    const product = this.products.find(p => p.id === id);
+  async getProductById(id) {
+    const products = JSON.parse(await fs.readFile('./productos.txt', 'utf-8'));
+    const product = products.find(producto => producto.id === id);
     if (product) {
-      return product;
+      console.log(product);
+    } else {
+      console.log("Producto no encontrado");
+    }
+  }
+
+  async updateProduct(id, updatedProduct) {
+    const productIndex = this.products.findIndex(p => p.id === id);
+    if (productIndex !== -1) {
+      this.products[productIndex] = { ...this.products[productIndex], ...updatedProduct };
+      await this.saveProductsToFile();
+      console.log('Producto actualizado correctamente:', this.products[productIndex]);
     } else {
       console.error('Producto no encontrado.');
     }
   }
+
+  async deleteProduct(id) {
+    const initialProductCount = this.products.length;
+    this.products = this.products.filter(p => p.id !== id);
+
+    if (this.products.length === initialProductCount) {
+      console.error('Producto no encontrado.');
+    } else {
+      await this.saveProductsToFile();
+      console.log('Producto eliminado correctamente.');
+    }
+  }
+
+  async saveProductsToFile() {
+    await fs.writeFile('./productos.txt', JSON.stringify(this.products));
+  }
 }
 
-// Crear una instancia de ProductManager con un array vacío de productos
-const manager = new ProductManager();
+// Cargar productos desde el archivo "productos.txt" al crear una instancia de ProductManager
+const loadProductsFromFile = async () => {
+  try {
+    const productsData = await fs.readFile('./productos.txt', 'utf-8');
+    const products = JSON.parse(productsData);
+    return new ProductManager(products);
+  } catch (error) {
+    return new ProductManager();
+  }
+};
 
-// Agregar productos
-manager.addProduct({
-  title: 'Producto 1',
-  description: 'Descripción del Producto 1',
-  price: 10.99,
-  thumbnail: 'ruta/imagen1.jpg',
-  code: 'P1',
-  stock: 50
+// Crear una instancia de ProductManager y cargar los productos desde el archivo
+loadProductsFromFile().then(async (manager) => {
+
+
+  // agregando productos
+  await manager.addProduct({
+    title: 'Producto 3',
+    description: 'Descripción del Producto 3',
+    price: 25.99,
+    thumbnail: 'ruta/imagen3.jpg',
+    code: 'P3',
+    stock: 30
+  });
+
+  await manager.addProduct({
+    title: 'Producto 2',
+    description: 'Descripción del Producto 6',
+    price: 256.99,
+    thumbnail: 'ruta/imagen3.jpg',
+    code: 'P5',
+    stock: 30
+  });
+
+  await manager.addProduct({   // Prueba de campos obligatorios.
+    title: 'Producto 1',
+    description: '',
+    price: 25.99,
+    thumbnail: 'ruta/imagen3.jpg',
+    code: 'P3',
+    stock: 30
+  });
+
+  await manager.getProductById(1); // Busca producto con ID 1
+
+  await manager.updateProduct(1, { title: 'Producto 1', price:'20000' }); // Actualiza producto con ID 1
+
+  //await manager.deleteProduct(1); // Eliminar producto con ID 1
+  const updatedProducts = manager.getProducts(); // Obtener todos los productos actualizados
+  console.log('Productos actualizados:', updatedProducts);
 });
-
-manager.addProduct({
-  title: 'Producto 2',
-  description: 'Descripción del Producto 2',
-  price: 19.99,
-  thumbnail: 'ruta/imagen2.jpg',
-  code: 'P2',
-  stock: 20
-});
-
-// Repito el "Code" para mostrar el error
-manager.addProduct({
-  title: 'Producto 3',
-  description: 'Descripción del Producto 3',
-  price: 29.99,
-  thumbnail: 'ruta/imagen2.jpg',
-  code: 'P2',
-  stock: 20
-});
-
-//Producto agregado sin descripcion para arrojar mensaje de campos obligatorios!
-manager.addProduct({
-  title: 'Producto 4',
-  description:"",
-  price: 29.99,
-  thumbnail: 'ruta/imagen4.jpg',
-  code: 'P4',
-  stock: 20
-});
-
-// Obtener todos los productos
-const allProducts = manager.getProducts();
-console.log('Todos los productos:', allProducts);
-
-// Obtener un producto por ID
-const productById = manager.getProductById(1); // ID válido
-console.log('Producto por ID:', productById);
-
-const nonExistingProduct = manager.getProductById(12345); // ID inválido
-// Mostrará en la consola: "Producto no encontrado."
