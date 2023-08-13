8f3bd35047f819c45a05dd2bd964b428bd63d5fb
fmalbran93 <fmalbran93@gmail.com>
2023-08-13 20:32:24 -0300

    actualizo proyecto

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import express from 'express';
-import { ProductManager } from './productManager.js';
+import { ProductManager } from './controllers/productManager.js';
 
 const app = express();
 const PORT = 4000;
 
-const manager = new ProductManager('./src/productos.txt');
+const filePath = './src/models/productos.txt'; // Ruta del archivo de productos
+const manager = new ProductManager(filePath);
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Para manejar el formato JSON en las solicitudes
 
 app.get('/', (req, res) => {
   res.send('Hola');
 });
 
 app.get('/productos/:id', async (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = req.params.id;
   const product = await manager.getProductById(id);
   if (product) {
     res.send(`Producto encontrado: ${JSON.stringify(product)}`);
@@ -22,14 +24,30 @@ app.get('/productos/:id', async (req, res) => {
   }
 });
 
+app.post('/productos', async (req, res) => {
+  const newProduct = req.body;
+  await manager.addProduct(newProduct);
+  res.json({ message: 'Producto agregado correctamente' });
+});
+
+app.put('/productos/:id', async (req, res) => {
+  const id = req.params.id;
+  const updatedProduct = req.body;
+  
+  await manager.updateProduct(id, updatedProduct);
+  res.json({ message: 'Producto actualizado correctamente' });
+});
+
+app.delete('/productos/:id', async (req, res) => {
+  const id = req.params.id;
+  
+  await manager.deleteProduct(id);
+  res.json({ message: 'Producto eliminado correctamente' });
+});
+
 app.get('/productos', async (req, res) => {
-  const { limit } = req.query;
   const products = await manager.getProducts();
-  if (limit) {
-    res.send(products.slice(0, limit));
-  } else {
-    res.send(products);
-  }
+  res.json(products);
 });
 
 app.get('*', (req, res) => {
